be6c6275d1dc2b7c3b83b7ec3f7ec2654d17658d
Markiyan333 <markiyan333@gmail.com>
2023-01-30 20:59:40 +0200

    Initial commit

---
+++ src/refs/index.js
@@ -0,0 +1,5 @@
+export const tbodyRef = document.querySelector(".js-tbody");
+export const inputRef = document.querySelector(".form-control");
+export const userTableRef = document.querySelector(".js-user-table");
+export const userAlbumRef = document.querySelector(".js-album");
+export const userListPhotos = document.querySelector(".js-list-song");
